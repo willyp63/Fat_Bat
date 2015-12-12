7633615393fafecfd791e166c9d38245a0ac9fa1
Wil Pirino <wpirino@wils-macbook-pro.local>
2015-12-12 14:43:12 -0500

    Initial Commit

---
+++ Fat Bat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fat Bat
+//
+//  Created by Wil Pirino on 12/12/15.
+//  Copyright © 2015 Wil Pirino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
